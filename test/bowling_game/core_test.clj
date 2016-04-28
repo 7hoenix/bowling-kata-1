bf3127711aea52d7dc2d03cb562a5ea50160bedb
Justin Holzmann <jphoenix@Justins-MacBook-Pro.local>
2016-04-28 13:51:45 -0500

    Refactors out duplication.

--- test/bowling_game/core_test.clj
+++ test/bowling_game/core_test.clj
@@ -2,10 +2,14 @@
   (:require [clojure.test :refer :all]
             [bowling-game.core :as game]))
 
+(defn- bowl-many [score-of-each-roll]
+  (reduce game/roll 0 (take 20 (repeat score-of-each-roll))))
+
 (deftest bowling-game
   (testing "test gutter game"
-    (let [score (reduce game/roll 0 (take 20 (repeat 0)))]
+    (let [score (bowl-many 0)]
       (is (= score 0))))
+
   (testing "test all ones"
-    (let [score (reduce game/roll 0 (take 20 (repeat 1)))]
+    (let [score (bowl-many 1)]
       (is (= score 20)))))
