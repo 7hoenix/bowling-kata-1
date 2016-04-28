a0c1865a7f42128a650b2470c50b025219261556
Justin Holzmann <jholzmann@8thlight.com>
2016-04-28 16:44:39 -0500

    Owl city... I don't understand lazy seq.

--- src/bowling_game/core.clj
+++ src/bowling_game/core.clj
@@ -6,5 +6,21 @@
 (defn roll [game pins] 
   (update game :rolls conj pins))
 
+(defn- spare? [rolls]
+  (= 10 (apply + (take 2 rolls))))
+
+(defn- score-frame-size [rolls]
+  (if (spare? rolls)
+    3
+    2))
+
+; => This function is an owl...
+(defn- frames [rolls]
+  (lazy-seq
+    (if (seq rolls)
+      (cons (vec (take (score-frame-size rolls) rolls))
+            (frames (drop 2 rolls)))
+      (seq ()))))
+
 (defn score [game] 
-  (reduce + 0 (:rolls game)))
+  (reduce (fn [total frame] (apply + total frame)) 0 (take 10 (frames (:rolls game)))))
