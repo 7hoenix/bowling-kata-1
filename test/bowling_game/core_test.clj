4c5a6225d8827ac8a7f1a05e486ed026cb7c2efe
Justin Holzmann <jholzmann@8thlight.com>
2016-04-28 13:56:33 -0500

    Changes roll-many function to include number of rolls.

--- test/bowling_game/core_test.clj
+++ test/bowling_game/core_test.clj
@@ -2,14 +2,14 @@
   (:require [clojure.test :refer :all]
             [bowling-game.core :as game]))
 
-(defn- bowl-many [score-of-each-roll]
-  (reduce game/roll 0 (take 20 (repeat score-of-each-roll))))
+(defn- roll-many[number-of-rolls score-of-each-roll]
+  (reduce game/roll 0 (take number-of-rolls (repeat score-of-each-roll))))
 
 (deftest bowling-game
   (testing "test gutter game"
-    (let [score (bowl-many 0)]
+    (let [score (roll-many 20 0)]
       (is (= score 0))))
 
   (testing "test all ones"
-    (let [score (bowl-many 1)]
+    (let [score (roll-many 20 1)]
       (is (= score 20)))))
