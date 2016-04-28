f1ff6ec856d8fc15b4cf3aea1b12cabbf35704bb
Justin Holzmann <jholzmann@8thlight.com>
2016-04-28 15:39:38 -0500

    Switches to a new board that keeps track of rolls
    Also changes over the tests so that the roll-many function works with
    new board.

--- src/bowling_game/core.clj
+++ src/bowling_game/core.clj
@@ -3,5 +3,8 @@
 (defn new-game []
   {:rolls []})
 
-(defn roll [score pins] 
-  (+ score pins))
+(defn roll [game pins] 
+  (update game :rolls conj pins))
+
+(defn score [game] 
+  (reduce + 0 (:rolls game)))
--- test/bowling_game/core_test.clj
+++ test/bowling_game/core_test.clj
@@ -2,22 +2,24 @@
   (:require [clojure.test :refer :all]
             [bowling-game.core :as game]))
 
-(defn- roll-many[number-of-rolls score-of-each-roll]
-  (reduce game/roll 0 (take number-of-rolls (repeat score-of-each-roll))))
+(defn- roll-many[game number-of-rolls score-of-each-roll]
+  (reduce (fn [game _] (game/roll game score-of-each-roll)) game (range number-of-rolls)))
 
 (deftest bowling-game
+  (let [g (game/new-game)]
   (testing "test gutter game"
-    (let [game (game/new-game)
-          score (roll-many 20 0)]
-      (is (= 0 score))))
+    (is (= 0
+           (-> g
+               (roll-many 20 0)
+               (game/score)))))
 
   (testing "test all ones"
-    (let [game (game/new-game)
-          score (roll-many 20 1)]
-      (is (= 20 score)))))
+    (is (= 20
+           (-> g
+               (roll-many 20 1)
+               (game/score)))))))
  
-; (testing "test roll one spare"
-;  (let [score (roll-many 3 5)]
-;    (is (= score 20)))))
-
-; Hmmmmm... I'm thinking i want a game now.
+  ; (testing "test roll one spare"
+  ;  (let [game (game/new-game)
+  ;        score (roll-many 3 5)]
+  ;    (is (= score 20))))))
